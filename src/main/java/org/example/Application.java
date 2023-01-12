8dfda3124293333abb361cc37d21be8d39d8868d
Nirup Murali <nirupg63@gmail.com>
2023-01-12 19:30:19 +0530

    init project

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.applet.Applet;
+
+public class Application {
+    public  static void  main(String[] args) {
+        System.out.println("Testing My app");
+    }
+}
---
+++ src/main/java/org/example/v1/Version1Tests.java
@@ -0,0 +1,7 @@
+package org.example.v1;
+
+public class Version1Tests {
+    public static void main(String args[]) {
+        System.out.println("This only showcases stuff from v1");
+    }
+}
